9b0f9737b69087c9e67f172a1eadba71322b4a39
xs <76900699+a293955@users.noreply.github.com>
2023-02-09 14:19:46 +0800

    Update vue.config.js

--- music-view/vue.config.js
+++ music-view/vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       "/api": {
-        target: "https://www.freemusic.ltd:8889/",
+        target: "",
         changeOrigin: true,
         pathRewrite: {
           "^/api": "",
