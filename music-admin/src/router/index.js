18f2e827b5d09c6e33bb5a686b0ecd750f72e744
xs <76900699+xs669@users.noreply.github.com>
2023-03-21 16:23:32 +0800

    Update index.js

--- music-admin/src/router/index.js
+++ music-admin/src/router/index.js
@@ -1,5 +1,8 @@
 import Vue from "vue";
-import Router from "vue-router";
+import VueRouter from "vue-router";
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+import getPageTitle from '@/utils/get-page-title'
 import LoginPage from "../pages/LoginPage";
 import Home from "../components/Home";
 import InfoPage from "../pages/InfoPage";
@@ -10,79 +13,139 @@ import SongPage from "../pages/SongPage";
 import ListSongPage from "../pages/ListSongPage";
 import Collect from "../pages/Collect";
 import Comment from "../pages/Comment";
+import axios from "axios";
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-export default new Router({
-  routes: [
-    {
-      path: "/",
-      component: LoginPage,
+const routes = [
+  {
+    path: "/login",
+    component: LoginPage,
+  },
+  {
+    path: "/",
+    component: Home,
+    redirect: '/Info',
+    meta: {
+      requireAuth: true,
     },
-    {
-      path: "/Home",
-      component: Home,
-      meta: {
-        requireAuth: true,
+    children: [
+      {
+        path: "/Info",
+        component: InfoPage,
+        meta: {
+          title: '系统首页',
+          requireAuth: true,
+        },
       },
-      children: [
-        {
-          path: "/Info",
-          component: InfoPage,
-          meta: {
-            requireAuth: true,
-          },
+      {
+        path: "/Consumer",
+        component: ConsumerPage,
+        meta: {
+          title: '用户管理',
+          requireAuth: true,
         },
-        {
-          path: "/Consumer",
-          component: ConsumerPage,
-          meta: {
-            requireAuth: true,
-          },
+      },
+      {
+        path: "/Singer",
+        component: SingerPage,
+        meta: {
+          title: '歌手管理',
+          requireAuth: true,
         },
-        {
-          path: "/Singer",
-          component: SingerPage,
-          meta: {
-            requireAuth: true,
-          },
+      },
+      {
+        path: "/SongList",
+        component: SongListPage,
+        meta: {
+          title: '歌单管理',
+          requireAuth: true,
         },
-        {
-          path: "/SongList",
-          component: SongListPage,
-          meta: {
-            requireAuth: true,
-          },
+      },
+      {
+        path: "/Song",
+        component: SongPage,
+        meta: {
+          title: '歌曲管理',
+          requireAuth: true,
         },
-        {
-          path: "/Song",
-          component: SongPage,
-          meta: {
-            requireAuth: true,
-          },
+      },
+      {
+        path: "/ListSong",
+        component: ListSongPage,
+        meta: {
+          title: '歌单歌曲管理',
+          requireAuth: true,
         },
-        {
-          path: "/ListSong",
-          component: ListSongPage,
-          meta: {
-            requireAuth: true,
-          },
+      },
+      {
+        path: "/collect",
+        component: Collect,
+        meta: {
+          title: '用户收藏管理',
+          requireAuth: true,
         },
-        {
-          path: "/collect",
-          component: Collect,
-          meta: {
-            requireAuth: true,
-          },
+      },
+      {
+        path: "/comment",
+        component: Comment,
+        meta: {
+          title: '用户评论管理',
+          requireAuth: true,
         },
-        {
-          path: "/comment",
-          component: Comment,
-          meta: {
-            requireAuth: true,
-          },
-        },
-      ],
-    },
-  ],
-});
+      },
+    ],
+  },
+]
+
+const router = new VueRouter({
+  mode: 'hash',
+  base: process.env.BASE_URL,
+  routes
+})
+
+NProgress.configure({ showSpinner: false })
+
+const whiteList = ['/login']
+
+router.beforeEach(async (to, from, next) => {
+  NProgress.start()
+  document.title = getPageTitle(to.meta.title)
+  if (to.meta.requireAuth) {
+    let token = localStorage.getItem("token")
+    let username = localStorage.getItem("username")
+    if (token) {
+      await axios.get(`/api/checkLoginState/${username}`).then(res => {
+        if (res.data.data) {
+          if (to.path === '/login') {
+            next({ path: '/' })
+            NProgress.done()
+          } else {
+            next()
+          }
+        } else {
+          localStorage.removeItem("token")
+          localStorage.removeItem("username")
+          next({path: '/login'})
+          NProgress.done()
+        }
+      })
+    } else {
+      if (whiteList.indexOf(to.path) !== -1) {
+        next()
+      } else {
+        next({path: '/login'})
+        NProgress.done()
+      }
+    }
+  } else {
+    next()
+  }
+
+})
+
+router.afterEach(() => {
+  NProgress.done()
+})
+
+export default router
