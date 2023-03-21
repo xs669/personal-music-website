5fcdafcf3dd21daa9b33f63a4c544dc179ade6bf
xs <76900699+xs669@users.noreply.github.com>
2023-03-21 18:10:58 +0800

    Update index.js

--- music-view/src/router/index.js
+++ music-view/src/router/index.js
@@ -1,5 +1,8 @@
 import Vue from "vue";
-import Router from "vue-router";
+import VueRouter from "vue-router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import getPageTitle from "@/utils/get-page-title";
 import Home from "@/pages/Home";
 import MyMusic from "../pages/MyMusic";
 import Singer from "../pages/Singer";
@@ -13,86 +16,143 @@ import SingerAblum from "../pages/SingerAlbum";
 import SongListAblum from "../pages/SongListAlbum";
 import LeaderBoard from "../pages/LeaderBoard";
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-// eslint-disable-next-line no-undef
-const originalPush = Router.prototype.push;
-
-// eslint-disable-next-line no-undef
-Router.prototype.push = function push(location) {
-  return originalPush.call(this, location).catch((err) => err);
-};
-
-export default new Router({
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home,
-    },
-    {
-      path: "/myMusic",
-      name: "myMusic",
-      component: MyMusic,
-      meta: {
-        requireAuth: true,
-      },
-    },
-    {
-      path: "/singer",
-      name: "singer",
-      component: Singer,
-    },
-    {
-      path: "/songList",
-      name: "songList",
-      component: SongList,
-    },
-    {
-      path: "/search",
-      name: "search",
-      component: Search,
-    },
-    {
-      path: "/lyric",
-      name: "lyric",
-      component: Lyric,
-    },
-    {
-      path: "/login",
-      name: "login",
-      component: Login,
-    },
-    {
-      path: "/register",
-      name: "register",
-      component: Register,
-    },
-    {
-      path: "/setting",
-      name: "setting",
-      component: Setting,
-      meta: {
-        requireAuth: true,
-      },
-    },
-    {
-      path: "/singerAlbum",
-      name: "singerAlbum",
-      component: SingerAblum,
-    },
-    {
-      path: "/songListAlbum",
-      name: "songListAlbum",
-      component: SongListAblum,
-    },
-    {
-      path: "/leaderBoard",
-      name: "leaderBoard",
-      component: LeaderBoard,
-    },
-  ],
-  scrollBehavior() {
-    return { x: 0, y: 0 };
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: Home,
   },
+  {
+    path: "/myMusic",
+    name: "myMusic",
+    component: MyMusic,
+    meta: {
+      title: '我的音乐',
+      requireAuth: true,
+    },
+  },
+  {
+    path: "/singer",
+    name: "singer",
+    component: Singer,
+    meta: {
+      title: '歌手',
+    },
+  },
+  {
+    path: "/songList",
+    name: "songList",
+    component: SongList,
+    meta: {
+      title: '歌单',
+    },
+  },
+  {
+    path: "/search",
+    name: "search",
+    component: Search,
+    meta: {
+      title: '搜索',
+    },
+  },
+  {
+    path: "/lyric",
+    name: "lyric",
+    component: Lyric,
+    meta: {
+      title: '歌词',
+    },
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+    meta: {
+      title: '登录',
+    },
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register,
+    meta: {
+      title: '注册',
+    },
+  },
+  {
+    path: "/setting",
+    name: "setting",
+    component: Setting,
+    meta: {
+      title: '个人中心',
+      requireAuth: true,
+    },
+  },
+  {
+    path: "/singerAlbum",
+    name: "singerAlbum",
+    component: SingerAblum,
+    meta: {
+      title: '歌手详情',
+    },
+  },
+  {
+    path: "/songListAlbum",
+    name: "songListAlbum",
+    component: SongListAblum,
+    meta: {
+      title: '歌单详情',
+    },
+  },
+  {
+    path: "/leaderBoard",
+    name: "leaderBoard",
+    component: LeaderBoard,
+    meta: {
+      title: '排行榜',
+    },
+  },
+];
+
+const router = new VueRouter({
+  mode: "hash",
+  base: process.env.BASE_URL,
+  routes,
 });
+
+NProgress.configure({ showSpinner: false });
+
+const whiteList = ["/login"];
+
+router.beforeEach(async (to, from, next) => {
+  NProgress.start();
+  document.title = getPageTitle(to.meta.title);
+  if (to.meta.requireAuth) {
+    let username = localStorage.getItem("consumerName");
+    if (username) {
+      if (to.path === "/login") {
+        next({ path: "/" });
+        NProgress.done();
+      } else {
+        next();
+      }
+    } else {
+      if (whiteList.indexOf(to.path) !== -1) {
+        next();
+      } else {
+        next({ path: "/login" });
+        NProgress.done();
+      }
+    }
+  } else {
+    next();
+  }
+});
+
+router.afterEach(() => {
+  NProgress.done();
+});
+
+export default router;
